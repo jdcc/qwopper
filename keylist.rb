596527a68e6b2da5e04935a962b6a67dee285e26
Justin Clark <JustinD.C.Clark@gmail.com>
2011-04-17 17:13:57 -0400

    Small bug fixes

--- keylist.rb
+++ keylist.rb
@@ -2,12 +2,11 @@ require './utils.rb'
 include Utils
 
 class KeyList
-	LIST_LENGTH = 10
-	PROB_LOWER_BOUND = 0.1
-	PROB_UPPER_BOUND = 0.2
+	LIST_LENGTH = 10000
+	PROB_LOWER_BOUND = 0.005
+	PROB_UPPER_BOUND = 0.015
 
-	FELL_PIXEL_X = 580
-	FELL_PIXEL_Y = 410
+	FELL_PIXEL = '580 410'
 	FELL_PIXEL_COLOR = '237 237 237'
 	FELL_DIST_IMAGE = '133x25+643+432'
 	TIME_BETWEEN_BINS = 0.01
@@ -43,10 +42,10 @@ class KeyList
 		@keylist.each_with_index do |keys, time|
 			keys.each do |key|
 				command = (key[1] == 'u' ? 'keyup' : 'keydown')
-				`xdotool #{command + ' --window ' + browserWindowId + ' --delay 0 ' + key[0]}`
+				`xdotool #{command + ' --window ' + browser_window_id + ' --delay 0 ' + key[0]}`
 			end
-			pixel_color = `./grabpixel "#{Utils::WINDOW_NAME}" #{FELL_PIXEL_X FELL_PIXEL_Y}`.strip
-			if  pixel_color == FELL_PIXEL_COLOR then
+			pixel_color = `./grabpixel "#{Utils::WINDOW_NAME}" #{FELL_PIXEL}`.strip
+			if pixel_color == FELL_PIXEL_COLOR then
 				distance = `xdotool search -name "#{Utils::WINDOW_NAME}" windowactivate \
 					&& xwd -name "#{Utils::WINDOW_NAME}" -out "Distance.xwd" \
 					&& convert Distance.xwd -negate -crop '#{FELL_DIST_IMAGE}' Distance#{time}.pnm \
