b8ab22eabcd85b11b1c5b1c9352ea86289f40130
Justin Clark <JustinD.C.Clark@gmail.com>
2011-04-17 23:45:49 -0400

    Added constants

--- utils.rb
+++ utils.rb
@@ -4,6 +4,10 @@ module Utils
 	BROWSER_COMMAND = 'google-chrome'
 	URL = 'http://www.foddy.net/Athletics.html'
 	APPLET_LOCATION = '720 450'
+	FELL_PIXEL = '580 410'
+	FELL_PIXEL_COLOR = '237 237 237'
+	JAW_PIXEL = '700 375'
+	JAW_PIXEL_RED_VALUE = 82
 
 	def prepare_applet
 		if `xdotool search -name "#{WINDOW_NAME}"`.strip == '' then
@@ -15,15 +19,15 @@ module Utils
 			`xdotool search -name "#{WINDOW_NAME}" windowactivate`
 		end
 
-		pixel_color = `./grabpixel "#{WINDOW_NAME}" 580 410`.strip
-		if  pixel_color == '237 237 237' then
+		pixel_color = `./grabpixel "#{WINDOW_NAME}" #{FELL_PIXEL}`.strip
+		if  pixel_color == FELL_PIXEL_COLOR then
 			`xdotool key q w o p`
 			`xdotool key --delay 0 space`
 		end
 		red, green, blue = 0
 		count = 0
-		until red.to_i == 82 do
-			head_pixel = `./grabpixel "#{WINDOW_NAME}" 700 375`
+		until red.to_i == JAW_PIXEL_RED_VALUE do
+			head_pixel = `./grabpixel "#{WINDOW_NAME}" #{JAW_PIXEL}`
 			red, green, blue = head_pixel.split(' ')
 			count += 1
 			if count == 30 then
