2f5098469fd0ae0fea3b0f66ea5ac6e1d3b74ef6
Justin Clark <JustinD.C.Clark@gmail.com>
2011-04-17 23:45:24 -0400

    Added constants

--- qwop.rb
+++ qwop.rb
@@ -1,16 +1,19 @@
 require "./generation.rb"
 
 class QWOP 
+	NO_GENERATIONS = 25
+	CONSISTENCY_TESTS = 5
+
 	def start
 		g = Generation.new
-		50.times do |i|
+		NO_GENERATIONS.times do |i|
 			puts 'Generation ' + i.to_s + ':'
 			`echo "#{'Generation ' + i.to_s + ':'}" >> test.log`
 			g.test
 			best = g.get_best
 			puts 'Best Distance: ' + best[:distance].to_s
 			`echo "#{'Best Distance: ' + best[:distance].to_s}" >> test.log`
-			5.times do 
+			CONSISTENCY_TESTS.times do 
 				distance, time = best[:keylist].test
 				puts distance
 				`echo "#{distance}" >> test.log`
